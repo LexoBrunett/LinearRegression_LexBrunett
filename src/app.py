be9d99ef6c35788465cbbc2c6d25cc5ef61b5beb
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2024-11-05 21:00:34 +0000

    linear regrression uploaded

--- src/app.py
+++ src/app.py
@@ -2,3 +2,84 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+import pandas as pd
+
+health_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/linear-regression-project-tutorial/main/medical_insurance_cost.csv")
+health_data = health_data.drop_duplicates().reset_index(drop = True)
+health_data.head(20)
+
+# Min-Max scaler
+
+from sklearn.preprocessing import MinMaxScaler
+
+health_data["sex_n"] = pd.factorize(health_data["sex"])[0]
+health_data["smoker_n"] = pd.factorize(health_data["smoker"])[0]
+health_data["region_n"] = pd.factorize(health_data["region"])[0]
+num_variables = ["age", "bmi", "children", "sex_n", "smoker_n", "region_n", "charges"]
+
+scaler = MinMaxScaler()
+scal_features = scaler.fit_transform(health_data[num_variables])
+total_data_scal = pd.DataFrame(scal_features, index = health_data.index, columns = num_variables)
+total_data_scal.head()
+
+# Feature selection
+
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.model_selection import train_test_split
+
+X = total_data_scal.drop("charges", axis = 1)
+y = total_data_scal["charges"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+selection_model = SelectKBest(f_regression, k = 4)
+selection_model.fit(X_train, y_train)
+
+selected_columns = X_train.columns[selection_model.get_support()]
+X_train_sel = pd.DataFrame(selection_model.transform(X_train), columns = selected_columns)
+X_test_sel = pd.DataFrame(selection_model.transform(X_test), columns = selected_columns)
+
+train_data = X_train_sel
+test_data = X_test_sel
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+fig, axis = plt.subplots(4, 2, figsize = (10, 14))
+total_data = pd.concat([train_data, test_data])
+
+sns.regplot(data = total_data, x = "age", y = "charges", ax = axis[0, 0])
+sns.heatmap(total_data[["charges", "age"]].corr(), annot = True, fmt = ".2f", ax = axis[1, 0], cbar = False)
+
+sns.regplot(data = total_data, x = "bmi", y = "charges", ax = axis[0, 1])
+sns.heatmap(total_data[["charges", "bmi"]].corr(), annot = True, fmt = ".2f", ax = axis[1, 1], cbar = False)
+
+sns.regplot(data = total_data, x = "children", y = "charges", ax = axis[2, 0])
+sns.heatmap(total_data[["charges", "children"]].corr(), annot = True, fmt = ".2f", ax = axis[3, 0], cbar = False)
+
+sns.regplot(data = total_data, x = "smoker_n", y = "charges", ax = axis[2, 1])
+sns.heatmap(total_data[["charges", "smoker_n"]].corr(), annot = True, fmt = ".2f", ax = axis[3, 1], cbar = False)
+
+plt.tight_layout()
+plt.show()
+
+X_train = train_data.drop(["charges"], axis = 1)
+y_train = train_data["charges"]
+X_test = test_data.drop(["charges"], axis = 1)
+y_test = test_data["charges"]
+
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+print(f"Intercep (a): {model.intercept_}")
+print(f"Coefficients (b1, b2): {model.coef_}")
+
+y_pred = model.predict(X_test)
+y_pred
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+print(f"MSE: {mean_squared_error(y_test, y_pred)}")
+print(f"R2 Score: {r2_score(y_test, y_pred)}")
